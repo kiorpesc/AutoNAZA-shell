a53036dd227a0e0457f4c5daacce020f0092f04a
Charles <ckiorpes@gmail.com>
2013-07-22 11:33:53 -0400

    Create cap_rename.sh
    This uses csete's capture program from his bonecam repository to capture video at hd or sd in raw form.

---
+++ cap_rename.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Capture raw video from the h264 webcam and write it to a file,
+# then rename the file to prevent overwrites
+
+FRAMES = $1
+RES = $2
+
+echo "Number of frames to capture: $FRAMES"
+echo "Resolution chosen: $RES"
+
+#set resolution to hd or sd
+if [ "$RES" == "hd" ]; then
+  v4l2-ctl --set-fmt-video=width=1920,height=1080,pixelformat=1
+else
+	v4l2-ctl --set-fmt-video=width=640,height=480,pixelformat=1
+
+#framerate defaults to 30
+v4l2-ctl --set-parm=30
+
+#begin capturing video
+echo "Capturing now..."
+./capture -c $1
+sleep 2
+
+#rename raw video file so it is not appended to or overwritten
+mv video.raw $(date +"%Y%m%d-%H%M")-webcam.raw
